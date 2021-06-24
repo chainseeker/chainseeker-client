27192345f3ad3896c8095a7d9f648e43718d220e
Vis Virial <visvirial@gmail.com>
2021-06-24 21:18:08 +0900

    Add putTransaction test (not tested).

---
+++ src/__tests__/putTransaction.ts
@@ -0,0 +1,38 @@
+
+import { Chainseeker } from '../index';
+const cs = new Chainseeker('https://tbtc.chainseeker.info/api');
+import * as bitcoin from 'bitcoinjs-lib';
+
+require('dotenv').config();
+
+const PRIVKEY = process.env['CS_PRIVKEY']!;
+
+test('put transaction', async () => {
+	const keyPair = bitcoin.ECPair.fromWIF(PRIVKEY, bitcoin.networks.testnet);
+	const { address } = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey, network: bitcoin.networks.testnet });
+	if(address === undefined) throw new Error('Failed to compute address.');
+	const utxos = await cs.getUtxos(address);
+	//console.log(utxos);
+	const psbt = new bitcoin.Psbt({ network: bitcoin.networks.testnet });
+	let balance = 0;
+	for(const utxo of utxos) {
+		balance += utxo.value;
+		psbt.addInput({
+			hash: utxo.txid,
+			index: utxo.vout,
+		});
+	}
+	psbt.addOutput({
+		address: address,
+		value: balance - 300 * utxos.length,
+	});
+	for(let vin=0; vin<utxos.length; vin++) {
+		psbt.signInput(vin, keyPair);
+	}
+	psbt.finalizeAllInputs();
+	const tx = psbt.extractTransaction();
+	//console.log(tx);
+	const txResult = await cs.putTransaction(tx.toHex());
+	expect(txResult.hash).toBe(tx.getId());
+});
+
