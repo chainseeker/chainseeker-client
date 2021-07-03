6ebd2730e942c124118b22f9796951fba12ad6c0
Vis Virial <visvirial@gmail.com>
2021-07-03 17:34:56 +0900

    Fix `getBlockWithTxids` test.

--- src/__tests__/getBlockWithTxids.ts
+++ src/__tests__/getBlockWithTxids.ts
@@ -7,11 +7,11 @@ const txids = ['0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098
 
 test('get block from block hash', async () => {
 	const block = await cs.getBlockWithTxids(blockHash);
-	expect(block.txids).toBe(txids);
+	expect(block.txids).toEqual(txids);
 });
 
 test('get block from block height', async () => {
 	const block = await cs.getBlockWithTxids(1);
-	expect(block.txids).toBe(txids);
+	expect(block.txids).toEqual(txids);
 });
 
